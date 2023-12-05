c997276ae0fc7609e6b0bdfd09e48ce5b8453087
Marian Kovalyshyn <mariankovalishin12345@gmail.com>
2023-12-05 22:46:20 +0200

    Select unique combinations of code1 & code2. Create dict with their corresponding sources.

--- main.py
+++ main.py
@@ -12,6 +12,25 @@ conditions = [
 ]
 bitCodeOne = (df["bitCode"] == 1).astype(object)
 bitCodeZero = (df["bitCode"] == 0).astype(object)
-bitCodeOne[bitCodeOne] = np.select(conditions, ["A", "B", "BpA"], default=np.nan)
-bitCodeZero[bitCodeZero] = np.select(conditions, ["H", "T", "TpH"], default=np.nan)
+bitCodeOne[bitCodeOne] = np.select(
+    conditions, ["A", "B", "BpA"], default=np.nan
+)
+bitCodeZero[bitCodeZero] = np.select(
+    conditions, ["H", "T", "TpH"], default=np.nan
+)
 df["siCode"] = np.where(df["bitCode"] == 1, bitCodeOne, bitCodeZero)
+
+possible_requests = (
+    df[["code1", "code2", "source"]]
+    .drop_duplicates()
+    .groupby(["code1", "code2"])["source"]
+    .agg(list)
+    .reset_index()
+)
+sources_for_requests = dict(
+    zip(
+        zip(possible_requests["code1"], possible_requests["code2"]),
+        possible_requests["source"],
+    )
+)
+print(len(sources_for_requests))
